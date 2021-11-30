2e0cc8106015a0f10284d91853c49fad4643da17
Elin Wrang <0301ELTY@gapp.uddevalla.se>
2021-11-30 17:48:26 +0100

    Created a flex layout and some buttons with functios.

---
+++ eventHandler.js
@@ -0,0 +1,8 @@
+function Forest() {
+    document.body.style.backgroundImage = "url('images/Kermit.jpg')";
+}
+
+function Room() {
+    document.body.style.backgroundImage = "url('images/SPRUCE2.jpg')";
+}
+    
